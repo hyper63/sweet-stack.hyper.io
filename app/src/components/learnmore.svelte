91628de7b3f135162a531e4636d5d2287d543471
Tom Wilson <tom@hyper.io>
2022-03-14 21:07:12 +0000

    updated learn more

--- app/src/components/learnmore.svelte
+++ app/src/components/learnmore.svelte
@@ -1,8 +1,40 @@
 <a id="learn-more" />
 <div class="hero min-h-screen bg-base-200">
   <div class="hero-content text-center">
-    <div class="max-w-md">
-      <h2 class="mt-8 text-5xl font-bold">Learn more</h2>
+    <div class="">
+      <h2 class="my-16 text-5xl font-bold">Learn more</h2>
+      <div class="flex space-x-16">
+        <div class="card w-96 bg-base-100 shadow-xl">
+          <div class="card-body">
+            <h3 class="card-title">What SWEET Stack is?</h3>
+            <ul class="mt-8 list-disc text-left">
+              <li>Simple</li>
+              <li>Svelte SPA</li>
+              <li>Deno (https)</li>
+              <li>GraphQL (API)</li>
+            </ul>
+          </div>
+        </div>
+        <div class="card w-96 bg-base-100 shadow-xl">
+          <div class="card-body">
+            <h3 class="card-title">What SWEET Stack is not?</h3>
+            <ul class="mt-8 list-disc text-left">
+              <li>No HMR</li>
+              <li>No SSR</li>
+              <li>No Magic</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <h4 class="mt-16 mb-4">Add DaisyUI</h4>
+      <div class="text-left mockup-code">
+        <pre
+          class="text-success"><code> &lt;link href="https://cdn.jsdelivr.net/npm/daisyui@2.8.0/dist/full.css" rel="stylesheet"&gt;&lt;/link&gt;
+        </code></pre>
+        <pre
+          class="text-success"><code> &lt;script src="https://cdn.tailwindcss.com"&gt;&lt;/script&gt;</code
+          ></pre>
+      </div>
       <a class="mt-16 btn" href="#">Back to Top</a>
     </div>
   </div>
